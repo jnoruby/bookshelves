92030a58b12daea32a9979db63ad3efe1712b25d
Michael Ruby <ruby@gen2.me>
2020-04-24 11:46:55 -0400

    goals in top-level comment

---
+++ bookshelves.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+• Analyse an image of a bookshelf containing books, segmenting it into images of
+  the individual books' spines.
+• Return OCR-generated text for each book spine, along with other identifying
+  data (geometric, colour, etc.)
+• Create database(s) of users' books.
+"""
+
+
+def bookshelves():
+    pass
+
+
+if __name__ == '__main__':
+    bookshelves()
