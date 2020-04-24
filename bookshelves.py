9e05639c8b87d84887197d6e45ab04fd1f7bbef3
Michael Ruby <ruby@gen2.me>
2020-04-24 12:50:49 -0400

    User path set; limit per imghdr

--- bookshelves.py
+++ bookshelves.py
@@ -12,12 +12,17 @@ import ux  # Functions for terminal user interaction. TODO after tests -> app
 
 
 def bookshelves():
-    # Retrieve any arguments from command line (path to image, verbose setting)
+    # Retrieve any arguments from command line (path to image, verbose setting).
     args = ux.get_args()
+
     # Set verbose boolean. Default to False if not set True in command line.
     v = args['verbose'] if args['verbose'] else False
+
     # Begin verbose reports if verbose set True.
     ux.print_program_introduction(args, v)
+
+    # Get path to image and confirm that it's a valid image with imghdr.
+    image_path, path_leaf = ux.get_image_path(args, v)
 
 
 if __name__ == '__main__':
--- ux.py
+++ ux.py
@@ -1,4 +1,5 @@
 import argparse
+import imghdr
 
 
 def get_args():
@@ -20,3 +21,50 @@ def print_program_introduction(args, v):
     if v:
         print(msg)
         print(f'Command-line arguments: {args}')
+
+
+def get_image_path(args, v):
+
+    def get_path_leaf(image_path):
+        # Fix Windows file paths:
+        image_path = image_path.replace('\\', '/')
+        # Extract path leaf.
+        path_leaf = image_path.split('/')[-1]
+        return path_leaf
+
+    if args['image'] is not None:
+        image_path = args['image']
+        argument_source = 'command-line argument'
+    else:
+        image_path = input(f'Please enter path to bookshelf image.\n Supported '
+                           f'file formats are defined by imghdr '
+                           f'{get_imghdr_supported_file_formats()}.\n'
+                           f'Path: ')
+        argument_source = 'runtime user input'
+    path_leaf = get_path_leaf(image_path)
+    if v:
+        if len(image_path) == 0:
+            print('No image file was defined. Program cannot continue.')
+            print('Exiting')
+            exit()
+        print(f'Testing path to bookshelf image {path_leaf}, defined by '
+              f'{argument_source}')
+    return image_path, path_leaf
+
+
+def get_imghdr_supported_file_formats():
+    """
+    Retrieves list of file formats that can be verified by imghdr, from the
+    module's own list of functions (if the variable name is unchanged), or from
+    a local string (accurate as of 2020-04-24) if the variable name changes.
+    :return: file formats verifiable by imghdr, space separated
+    """
+    supported_types = ''
+    try:
+        for test in imghdr.tests:
+            supported_types += test.__name__[5:]  # Ignoring prefix 'test_'
+            supported_types += ' '
+    except AttributeError:
+        supported_types = ('jpeg png gif tiff rgb bpm pgm ppm rast xbm bmp '
+                           'webp exr')
+    return supported_types.strip()
